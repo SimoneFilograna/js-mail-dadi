884833136cf5b725ad1351837e0bc5d88b448c69
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-05 15:52:42 +0200

    finished-without-bonus

---
+++ js/dadi-main.js
@@ -0,0 +1,25 @@
+const userNumber = Math.floor((Math.random() * 6) +1);
+console.log(userNumber)
+
+const iaNumber = Math.floor((Math.random() * 6) +1);
+console.log(iaNumber)
+
+const btnRoll = document.getElementById("btn")
+
+
+btnRoll.addEventListener("click", function(){
+
+    //print valori generati
+    document.getElementById("my-number").innerHTML = userNumber;
+    document.getElementById("ia-number").innerHTML = iaNumber; 
+
+    if ( userNumber > iaNumber){
+        document.getElementById("outcome").innerHTML = "COMPLIMENTI HAI VINTO!!!!"
+    } else if ( userNumber == iaNumber) {
+        document.getElementById("outcome").innerHTML = "PAREGGIO, RIPROVIAMO"
+    }  else {
+        document.getElementById("outcome").innerHTML = "MI DISPIACE HAI PERSO, SARA' PER LA PROSSIMA VOLTA"
+    }
+})
+
+
